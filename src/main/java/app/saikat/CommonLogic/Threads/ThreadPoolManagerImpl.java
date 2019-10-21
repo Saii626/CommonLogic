8257b401b227ad7662fda0374a416ba92fecbc03
Saikat Dey <saikat626@gmail.com>
2019-10-21 23:45:20 +0530

    Removed comments

--- src/main/java/app/saikat/CommonLogic/Threads/ThreadPoolManagerImpl.java
+++ src/main/java/app/saikat/CommonLogic/Threads/ThreadPoolManagerImpl.java
@@ -59,81 +59,11 @@ class ThreadPoolManagerImpl implements ThreadPoolManager {
         return executor.submit(callable);
     }
 
-    // @Override
-    // public RunnableState cancel(Runnable runnable, boolean shouldInterrupt, String name)
-    //         throws NoSuchThreadPoolException, NoSuchRunnableException {
-    //     Tuple<RunnableHolder, RunnableState> info;
-    //     ThreadPoolExecutor executor = getThreadPool(name);
-    //     // executor.
-    //     // executor.submit()
-
-    //     synchronized (runnableList) {
-    //         info = runnableList.stream()
-    //                 .filter(entry -> entry.first.get()
-    //                         .equals(runnable))
-    //                 .findFirst()
-    //                 .orElseThrow(() -> new NoSuchRunnableException(runnable, name));
-    //     }
-
-    //     synchronized (info) {
-    //         switch (info.second) {
-    //             case EXECUTING:
-    //                 if (shouldInterrupt) info.first.getRunningOnThread().interrupt();
-    //             case WAITING:
-    //                 info.second = RunnableState.CANCELLED;
-    //                 break;
-    //             case CANCELLED:
-    //             case COMPLETED:
-    //                 break;
-    //         }
-    //     }
-
-    //     return info.second;
-    // }
-
-    // @Override
-    // public RunnableState getStateOf(Runnable runnable, String name) throws NoSuchThreadPoolException, NoSuchRunnableException {
-    //     return null;
-    // }
-
     @Override
     public boolean allocateThreadPoolIfNotPresent(int coreThreads, int maxThreads, long ttl, String name) {
         poolConfig.addToThreadPool(name, new Config(coreThreads, maxThreads, ttl));
         return false;
     }
-
-    // private Runnable wrap(Tuple<RunnableHolder, RunnableState> info) {
-    //     return new Runnable() {
-
-    //         @Override
-    //         public void run() {
-    //             synchronized (info) {
-    //                 // If completed or canceled, return inmmediately
-    //                 if (info.second.isTerminal()) return;
-
-    //                 info.second = RunnableState.EXECUTING;
-    //                 info.first.setRunningOnThread(Thread.currentThread());
-    //                 info.notifyAll();
-    //             }
-
-    //             try {
-    //                 info.first.get()
-    //                         .run();
-    //             } finally {
-
-    //                 synchronized (info) {
-
-    //                     // If not canceled already, change the status to completed
-    //                     if (!info.second.isTerminal()) info.second = RunnableState.COMPLETED;
-
-    //                     info.first.convertToWeakRef();
-    //                     info.first.setRunningOnThread(null);
-    //                     info.notifyAll();
-    //                 }
-    //             }
-    //         }
-    //     };
-    // }
 
     private ThreadPoolExecutor getThreadPool(String name) throws NoSuchThreadPoolException {
         if (!threadPools.containsKey(name))
@@ -141,91 +71,6 @@ class ThreadPoolManagerImpl implements ThreadPoolManager {
 
         return threadPools.get(name);
     }
-
-    // @Override
-    // public void execute(Runnable runnable) {
-    //     execute(runnable, "global");
-    // }
-
-    // @Override
-    // public void execute(Runnable runnable, String name) {
-    //     Tuple<RunnableHolder, RunnableState> info = Tuple.of(RunnableHolder.of(runnable), RunnableState.WAITING);
-
-    //     synchronized (runnableList) {
-    //         runnableList.add(info);
-    //     }
-    //     threadPools.get(name).execute(() -> {
-    //         synchronized (info) {
-    //             info.second = RunnableState.EXECUTING;
-    //             info.notifyAll();
-    //         }
-
-    //         runnable.run();
-
-    //         synchronized (info) {
-    //             info.second = RunnableState.COMPLETE;
-    //             info.first.convertToWeakRef();
-    //             info.notifyAll();
-    //         }
-    //     });
-    // }
-
-    // @Override
-    // public void waitFor(Runnable runnable) throws NoSuchElementException {
-    //     Tuple<RunnableHolder, RunnableState> infoTuple;
-    //     synchronized (runnableList) {
-    //         infoTuple = runnableList.stream().filter(t -> t.first.get().equals(runnable)).findAny()
-    //                 .orElseThrow(() -> new NoSuchElementException("Runnable {} not posted for execution"));
-    //     }
-
-    //     if (infoTuple.second.equals(RunnableState.COMPLETE))
-    //         return;
-
-    //     synchronized (infoTuple) {
-    //         while (!infoTuple.second.equals(RunnableState.COMPLETE)) {
-    //             try {
-    //                 infoTuple.wait(1000);
-    //                 logger.warn("Still executing {}", runnable);
-    //             } catch (InterruptedException e) {
-    //                 logger.error("Error: {}", e);
-    //             }
-    //         }
-    //     }
-    // }
-
-    // @Override
-    // public boolean waitFor(Runnable runnable, long millis) {
-    //     Tuple<RunnableHolder, RunnableState> infoTuple;
-    //     synchronized (runnableList) {
-    //         infoTuple = runnableList.stream().filter(t -> t.first.get().equals(runnable)).findAny()
-    //                 .orElseThrow(() -> new NoSuchElementException("Runnable {} not posted for execution"));
-    //     }
-
-    //     if (infoTuple.second.equals(RunnableState.COMPLETE))
-    //         return true;
-
-    //     synchronized (infoTuple) {
-    //         long beginTime =
-    //         while (!infoTuple.second.equals(RunnableState.COMPLETE)) {
-    //             try {
-    //                 infoTuple.wait(millis);
-    //                 logger.warn("Still executing {}", runnable);
-    //             } catch (InterruptedException e) {
-    //                 logger.error("Error: {}", e);
-    //             }
-    //         }
-    //     }
-    // }
-
-    // @Override
-    // public void allocateThreadPoolIfNotPresent(int coreThreads, int maxThreads, String name) {
-
-    // }
-
-    // @Override
-    // public RunnableState getStateOf(Runnable runnable) {
-    //     return null;
-    // }
 
     @Provides
     public static ThreadPoolManager getThreadPoolManager(ThreadPoolConfig config) {
